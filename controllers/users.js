220a81aac54c374a3e6602c299da74b93e46f3c9
Natali Kamenkova <natalikamenkova@gmail.com>
2022-12-11 09:22:09 +0200

    git rm -rf --cached

--- controllers/users.js
+++ controllers/users.js
@@ -3,18 +3,18 @@ const User = require('../models/user');
 module.exports.getUsers = (req, res) => {
   User.find({})
     .then((users) => res.send(users))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .catch(() => res.status(500).send({ message: 'Ошибка сервера' }));
 };
 module.exports.getUserById = (req, res) => {
   const { userId } = req.params;
   User.findById(userId)
     .then((user) => res.send(user))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .catch(() => res.status(500).send({ message: 'Ошибка сервера' }));
 };
 
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
     .then((user) => res.status(201).send(user))
-    .catch(() => res.status(500).send({ message: "error" }));
+    .catch(() => res.status(500).send({ message: 'Ошибка сервера' }));
 };
