266a49c611e8f71bf78cfed5f4d95c37fb544540
Natali Kamenkova <natalikamenkova@gmail.com>
2023-04-09 20:40:45 +0300

    Revert "test"
    This reverts commit fe33ad2b15cea28fcad8bc103b30316f5f7af5c5.

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -7,17 +7,17 @@ const { JWT_SECRET = 'dev-key' } = process.env;
 module.exports.auth = (req, res, next) => {
   const { authorization } = req.headers;
 
-  if (!authorization /*|| !authorization.startsWith('Bearer ')*/) {
-    throw new NotAuthError('Необходима авторизация1');
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    throw new NotAuthError('Необходима авторизация');
   }
 
-  const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDMyZjU1NTkxN2ZhNDAxYWMyMzI1N2YiLCJpYXQiOjE2ODEwNjEyMDYsImV4cCI6MTY4MTY2NjAwNn0.tIokIMHPHHyiwdtj_js0rS7Wg7IL-_I__U3eRf36uKo";
+  const token = authorization.replace('Bearer ', '');
   let payload;
 
   try {
     payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
-    next(new NotAuthError('Необходима авторизация2'));
+    next(new NotAuthError('Необходима авторизация'));
   }
 
   req.user = payload; // записываем пейлоуд в объект запроса
