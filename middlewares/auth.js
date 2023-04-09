a1b22cba938ad40d337755e22fe140549d0ee1e3
Natali Kamenkova <natalikamenkova@gmail.com>
2023-04-09 20:23:35 +0300

    Revert "f"
    This reverts commit e24d950a8392128a36e2ab9290c877f6dc38f0b7.

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -7,22 +7,17 @@ const { JWT_SECRET = 'dev-key' } = process.env;
 module.exports.auth = (req, res, next) => {
   const { authorization } = req.headers;
 
-  if (!authorization/* || !authorization.startsWith('Bearer ')*/) {
-    throw new NotAuthError('Необходима авторизация 1');
-  }
-  let token = ""
-  if (authorization.startsWith('Bearer ')) {
-    token = authorization.replace('Bearer ', '');
-  } else {
-    authorization;
-  }
+  /*if (!authorization || !authorization.startsWith('Bearer ')) {
+    throw new NotAuthError('Необходима авторизация');
+  }*/
 
+  //const token = authorization.replace('Bearer ', '');
   let payload;
 
   try {
-    payload = jwt.verify(token, JWT_SECRET);
+    payload = jwt.verify(authorization, JWT_SECRET);
   } catch (err) {
-    next(new NotAuthError('Необходима авторизация 2'));
+    next(new NotAuthError('Необходима авторизация...'));
   }
 
   req.user = payload; // записываем пейлоуд в объект запроса
